12fe728fb7015c65d98602ce6cae2096c3fee2ac
gompertzmakeham <46943384+gompertzmakeham@users.noreply.github.com>
2019-02-08 16:03:44 -0700

    Test for interval boundary weirdness
    ...if I had had these tests in here in the first place I would have saved myself a week of debugging...

---
+++ tests/census/personcensus.sql
@@ -0,0 +1,59 @@
+WITH
+	intervaldata AS
+	(
+		SELECT
+			a0.uliabphn,
+			a0.cornercase,
+			a0.durationstart,
+			a0.durationend,
+			a0.intervalfirst,
+			a0.intervallast,
+			a0.intervalbirth,
+			a0.intervaldeceased,
+			a0.intervalimmigrate,
+			a0.intervalemigrate,
+			CASE a0.intervalorder
+				WHEN 1 THEN
+					1
+				ELSE
+					0
+			END orderfirst,
+			CASE a0.intervalorder
+				WHEN a0.intervalcount THEN
+					1
+				ELSE
+					0
+			END orderlast
+		FROM
+			personcensus a0
+	)
+SELECT
+	a0.intervalfirst,
+	a0.orderfirst,
+	a0.intervalbirth,
+	a0.intervalimmigrate,
+	a0.intervallast,
+	a0.orderlast,
+	a0.intervaldeceased,
+	a0.intervalemigrate,
+	COUNT(*) intervals
+FROM
+	intervaldata a0
+GROUP BY
+	a0.intervalfirst,
+	a0.orderfirst,
+	a0.intervalbirth,
+	a0.intervalimmigrate,
+	a0.intervallast,
+	a0.orderlast,
+	a0.intervaldeceased,
+	a0.intervalemigrate
+ORDER BY
+	1 DESC NULLS FIRST,
+	2 DESC NULLS FIRST,
+	3 DESC NULLS FIRST,
+	4 DESC NULLS FIRST,
+	5 DESC NULLS FIRST,
+	6 DESC NULLS FIRST,
+	7 DESC NULLS FIRST,
+	8 DESC NULLS FIRST;
